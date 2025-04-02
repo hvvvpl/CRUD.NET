4440c46ba1bffa83df08e9c49e95df6872a080a7
GABRIEL <GABRIEL@AMAQUINA>
2025-04-02 14:46:07 -0300

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtNome = new TextBox();
+            txtTelefone = new TextBox();
+            txtEmail = new TextBox();
+            button1 = new Button();
+            mySqlCommand1 = new MySql.Data.MySqlClient.MySqlCommand();
+            listContatos = new ListView();
+            label4 = new Label();
+            txtBuscar = new TextBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(20, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(20, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Telefone:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 116);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 15);
+            label3.TabIndex = 2;
+            label3.Text = "E-mail:";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(12, 27);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(194, 23);
+            txtNome.TabIndex = 3;
+            // 
+            // txtTelefone
+            // 
+            txtTelefone.Location = new Point(12, 80);
+            txtTelefone.Name = "txtTelefone";
+            txtTelefone.Size = new Size(194, 23);
+            txtTelefone.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(12, 134);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(194, 23);
+            txtEmail.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(97, 181);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 27);
+            button1.TabIndex = 6;
+            button1.Text = "Salvar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // mySqlCommand1
+            // 
+            mySqlCommand1.CacheAge = 0;
+            mySqlCommand1.Connection = null;
+            mySqlCommand1.EnableCaching = false;
+            mySqlCommand1.Transaction = null;
+            // 
+            // listContatos
+            // 
+            listContatos.Location = new Point(233, 56);
+            listContatos.Name = "listContatos";
+            listContatos.Size = new Size(418, 148);
+            listContatos.TabIndex = 7;
+            listContatos.UseCompatibleStateImageBehavior = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(232, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(91, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Buscar Contato:";
+            // 
+            // txtBuscar
+            // 
+            txtBuscar.Location = new Point(232, 27);
+            txtBuscar.Name = "txtBuscar";
+            txtBuscar.Size = new Size(338, 23);
+            txtBuscar.TabIndex = 9;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(576, 26);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 10;
+            button2.Text = "Buscar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(663, 222);
+            Controls.Add(button2);
+            Controls.Add(txtBuscar);
+            Controls.Add(label4);
+            Controls.Add(listContatos);
+            Controls.Add(button1);
+            Controls.Add(txtEmail);
+            Controls.Add(txtTelefone);
+            Controls.Add(txtNome);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtNome;
+        private TextBox txtTelefone;
+        private TextBox txtEmail;
+        private Button button1;
+        private MySql.Data.MySqlClient.MySqlCommand mySqlCommand1;
+        private ListView listContatos;
+        private Label label4;
+        private TextBox txtBuscar;
+        private Button button2;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,132 @@
+using MySql.Data.MySqlClient;  // diretiva do pacote NuGet MySql.Data
+
+
+namespace WinFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        private MySqlConnection Conexao;            // propriedade que conecta o programa com o MySql pelo pacote NuGet
+        private string data_source = "datasource=localhost; username=root; password=; database=db_agenda";          // fonte de dados
+        public Form1()
+        {
+            InitializeComponent();
+
+            listContatos.View = View.Details;   // Define o modo de exibição do ListView para 'Details', exibindo itens em uma grade com colunas
+            listContatos.LabelEdit = true;      // Permite a edição do texto do item, mesmo que a funcionalidade de edição ainda não exista no aplicativo
+            listContatos.AllowColumnReorder = true;     // Permite a reordenação das colunas
+            listContatos.FullRowSelect = true;      // Seleciona a linha inteira ao clicar em um item
+            listContatos.GridLines = true;      // Exibe linhas de grade no ListView
+
+            listContatos.Columns.Add("ID", 30, HorizontalAlignment.Left); // Adiciona a coluna 'ID'
+            listContatos.Columns.Add("Nome", 150, HorizontalAlignment.Left); // Adiciona a coluna 'Nome'
+            listContatos.Columns.Add("E-mail", 150, HorizontalAlignment.Left); // Adiciona a coluna 'E-mail'
+            listContatos.Columns.Add("Telefone", 150, HorizontalAlignment.Left); // Adiciona a coluna 'Telefone'
+
+        }
+
+        /// <summary>
+        /// 
+        /// instancia objeto conexao com a 'fonte dos dados, usuario, senha e destino'.
+        /// cria o comando INSERT na variavel 'sql'.
+        /// cria o objeto comando, com o código 'sql' e a 'Conexao' a ser usada como parâmetro.
+        /// 
+        /// </summary>
+        /// <param name="sender">button1_Click</param>
+        /// <param name="e"></param>
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // criar conexão com MySql usando a propriedade criada na classe
+                Conexao = new MySqlConnection(data_source);         // objeto conexao com a fonte de dados como parâmetro
+
+                string sql = "INSERT INTO contato (nome, telefone, email)" +
+                           $" VALUES ('{txtNome.Text}','{txtTelefone.Text}','{txtEmail.Text}')";
+
+                // Executar comando INSERT
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+
+                Conexao.Open();
+
+                comando.ExecuteReader();
+
+                MessageBox.Show("Contato adicionado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// cria uma string 'q' com o que foi inserido no textbox txtBuscar
+        /// conecta ao banco de dados
+        /// cria a sentença sql com a busca do usuario
+        /// cria o objeto comando com o código e a conexão a serem usadas
+        /// cria o objeto reader para ler o resultado do comando e limpa a ListView listContatos caso já tenha dados anteriores   
+        /// pega todos os resultados do comando sql e organiza em um vetor de strings e exibe na ListView listContatos
+        /// 
+        /// </summary>
+        /// <param name="sender">button2_Click</param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string q = "'%" + txtBuscar.Text + "%'";         //deve colocar aspas simples para o MySql entender como string 
+
+                // criar conexão com MySql 
+                Conexao = new MySqlConnection(data_source);
+
+                string sql = "SELECT * FROM contato " +
+                            "WHERE nome LIKE " + q + " OR email LIKE " + q;
+
+
+                // Executar comando INSERT
+                Conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                listContatos.Items.Clear();
+
+                while (reader.Read())       //retorna true sempre que houver mais registros para ler
+                {
+                    string[] linha =        //armazena em um novo vetor os dados dos campos 0,1,2,3 da tabela contato
+                    {
+                        reader[0].ToString(),       // Converte qualquer tipo para string
+                        reader[1].ToString(),
+                        reader[2].ToString(),
+                        reader[3].ToString(),
+                        /*
+                        reader.GetString(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                        dessa forma, funcioona porém esse método considera que o dado buscado já seja string, e se tiver int dá erro
+                        */
+                    };
+                    var linhaListView = new ListViewItem(linha);            //cria um objeto ListViewItem com os dados da linha
+
+                    listContatos.Items.Add(linhaListView);          //exibe a variavel linhaListView na ListView listContatos
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+    }
+}
